56f3a9b9a92d2838ecc87a1a08db3cfd512882f3
Sonic <gorbovtsovasofija@mail.com>
2026-07-01 20:18:07 +0300

    fix checking_instruments.sh

--- checking_instruments.sh
+++ checking_instruments.sh
@@ -2,12 +2,11 @@
 
 sudo apt update
 
-sudo apt install -y
-    build-essential 
-    qtbase5-dev 
-    qtbase5-dev-tools 
-    libqt5core5a 
-    ibqt5gui5 
-    libqt5widgets5
-    qt5-qmake
+sudo apt install -y   \
+    cmake             \
+    build-essential   \
+    qtbase5-dev       \
+    qtbase5-dev-tools \
+    libqt5widgets5    \
+    qt5-qmake         \
     qtbase5-dev-tools
